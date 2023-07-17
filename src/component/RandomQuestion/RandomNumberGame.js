0c7b4dc0a53963eb0c34a0bc47a8aefb559e768a
FaizShaikh0705 <faizshaikh0705@gmail.com>
2023-07-17 14:22:33 +0530

    added data section

--- src/component/RandomQuestion/RandomNumberGame.js
+++ src/component/RandomQuestion/RandomNumberGame.js
@@ -2,62 +2,88 @@ import { useState } from 'react';
 import { Container, Row } from 'react-bootstrap';
 import Card from 'react-bootstrap/Card';
 import questions from './questions';
+import convoQuestions from './convoQuestion';
+import pastQuestions from './pastQuestion';
 import Button from 'react-bootstrap/Button';
-import './RandomNumberGame.css'
-
+import './RandomNumberGame.css';
 
 const RandomNumberGame = () => {
-    const [numbers, setNumbers] = useState([...Array(100)].map((_, index) => index + 1));
-    const [currentNumber, setCurrentNumber] = useState(null);
-    const [currentQuestion, setCurrentQuestion] = useState('');
+  const [dataType, setDataType] = useState('general');
+  const [currentQuestion, setCurrentQuestion] = useState('');
 
-    const getNextNumber = () => {
-        if (numbers.length === 0) {
-            setCurrentNumber(null);
-            setCurrentQuestion('');
-            return;
-        }
+  const handleDataTypeChange = (event) => {
+    setDataType(event.target.value);
+  };
 
-        const randomIndex = Math.floor(Math.random() * numbers.length);
-        const number = numbers[randomIndex];
-        const updatedNumbers = [...numbers];
-        updatedNumbers.splice(randomIndex, 1);
+  const getRandomQuestion = () => {
+    let selectedQuestions = [];
 
-        setNumbers(updatedNumbers);
-        setCurrentNumber(number);
+    if (dataType === 'general') {
+      selectedQuestions = questions;
+    } else if (dataType === 'convo') {
+      selectedQuestions = convoQuestions;
+    } else if (dataType === 'past') {
+      selectedQuestions = pastQuestions;
+    }
 
-        // Generate a random question based on the number
-        const question = questions[number];
-        setCurrentQuestion(question);
+    if (selectedQuestions.length === 0) {
+      setCurrentQuestion('');
+      return;
+    }
 
-    };
-    return (
-        <>
-            <div>
-                <Container>
-                    <Row className='d-flex justify-content-center'>
-                        <Button className='genarate-button' variant="dark" onClick={getNextNumber}>Generate Random Number</Button>
-                        {currentNumber && (
-                            <div className='d-flex justify-content-center'>
-                                <Card className='cards' style={{ width: '18rem' }}>
-                                    <Card.Img variant="top" src="./images/ans.jpg" />
-                                    <Card.Body>
-                                        <Card.Title>Random Number: {currentNumber}</Card.Title>
-                                        <Card.Text>
-                                            {currentQuestion}
-                                        </Card.Text>
-                                    </Card.Body>
-                                </Card>
-                            </div>
-                        )}
-                        {!currentNumber &&
-                            <div className='d-flex justify-content-center'><p>Game over! All numbers have been used.</p></div>
-                        }
-                    </Row>
-                </Container>
+    const randomIndex = Math.floor(Math.random() * selectedQuestions.length);
+    const question = selectedQuestions[randomIndex];
+
+    setCurrentQuestion(question);
+  };
+
+  const remainingCount = () => {
+    if (dataType === 'general') {
+      return questions.length;
+    } else if (dataType === 'convo') {
+      return convoQuestions.length;
+    } else if (dataType === 'past') {
+      return pastQuestions.length;
+    }
+  };
+
+  return (
+    <div>
+      <Container>
+        <Row className='d-flex justify-content-center'>
+          <label htmlFor="data-type-select">Select Data Type:</label>
+          <select id="data-type-select" value={dataType} onChange={handleDataTypeChange}>
+            <option value="general">General</option>
+            <option value="convo">Conversation</option>
+            <option value="past">Past</option>
+          </select>
+          <Button className='generate-button mt-4' variant="dark" onClick={getRandomQuestion}>
+            Generate Random Question
+          </Button>
+          {currentQuestion && (
+            <div className='d-flex justify-content-center'>
+              <Card className='cards' style={{ width: '18rem' }}>
+                {/* Update the image URL below */}
+                <Card.Img variant="top" src="./images/ans.jpg" />
+                <Card.Body>
+                  <Card.Title>Random Question:</Card.Title>
+                  <Card.Text>{currentQuestion}</Card.Text>
+                </Card.Body>
+              </Card>
             </div>
-        </>
-    )
-}
+          )}
+          {!currentQuestion && (
+            <div className='d-flex justify-content-center'>
+              <p>Game over! All questions have been used.</p>
+            </div>
+          )}
+          <div>
+            <p className='d-flex justify-content-center m-4'>Questions Left: {remainingCount()}</p>
+          </div>
+        </Row>
+      </Container>
+    </div>
+  );
+};
 
-export default RandomNumberGame
+export default RandomNumberGame;
